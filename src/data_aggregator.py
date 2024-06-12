3733e8dafc6c7d593b28e348cd87fd80788b5ad0
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2024-06-12 17:25:26 -0300

    Modifications: src/data_aggregator.py

--- src/data_aggregator.py
+++ src/data_aggregator.py
@@ -3,8 +3,8 @@ import os
 import logging
 
 class DataAggregator:
-    def __init__(self, grants_file_path):
-        self.grants_file_path = grants_file_path
+    def __init__(self, found_file_path):
+        self.found_file_path = found_file_path
         self.logger = logging.getLogger(__name__)
         self.run_number = int(os.getenv('GITHUB_RUN_NUMBER', 0))
         self.query_folder = self.run_number % int(os.getenv('NUMBER_OF_QUERIES', 1))
@@ -18,26 +18,26 @@ class DataAggregator:
             for link in links:
                 file.write('\n' + link)
 
-    def read_grants_data(self):
-        if os.path.exists(self.grants_file_path) and os.path.getsize(self.grants_file_path) > 0:
-            with open(self.grants_file_path, 'r') as file:
+    def read_found_data(self):
+        if os.path.exists(self.found_file_path) and os.path.getsize(self.found_file_path) > 0:
+            with open(self.found_file_path, 'r') as file:
                 return json.load(file)
         else:
             return {}
 
-    def write_grants_data(self, data):
-        with open(self.grants_file_path, 'w') as file:
+    def write_found_data(self, data):
+        with open(self.found_file_path, 'w') as file:
             json.dump(data, file, indent=4)
 
-    def add_grant_data(self, query_data, links):
+    def add_found_data(self, query_data, links):
         self.append_links_to_file(links)
-        grants_data = self.read_grants_data()
+        found_data = self.read_found_data()
         
-        query_key = query_data['query']
-        if query_key not in grants_data:
-            grants_data[query_key] = []
+        query_key = self.query_folder
+        if query_key not in found_data:
+            found_data[query_key] = []
 
-        grants_data[query_key].extend(links)
+        found_data[query_key].extend(links)
         
-        self.write_grants_data(grants_data)
-        return grants_data
+        self.write_found_data(found_data)
+        return found_data
