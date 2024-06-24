f016e9c2b223fe315d419973edbc140d8b15235e
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2024-06-23 21:19:29 -0300

    Added scripts for easy and fast data maintaining

--- Scripts/schedule_updates.sh
+++ Scripts/schedule_updates.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Path to the reviewed_links_update.sh script
-SCRIPT_PATH="./reviewed_links_update.sh"
-
-# Function to call the reviewed_links_update.sh script
-run_update_script() {
-  bash "$SCRIPT_PATH"
+# Function to run a script and echo its name and the current date
+run_script() {
+  echo "Running $1: $(date)"
+  bash "$1" | while IFS= read -r line; do
+    echo "[$(date)] $1: $line"
+  done
 }
 
 # Main loop
 while true; do
-  echo "Running reviewed_links_update.sh: $(date)"
-  run_update_script
+  # Run the reviewed_links_update.sh script
+  run_script "./reviewed_links_update.sh"
+
+  # Run the update_search_queries.sh script
+  run_script "./update_search_queries.sh"
+
+  # Run the queries_clean_up.sh script
+  run_script "./queries_clean_up.sh"
+
+  # Wait for 15 minutes before the next update
   echo "Waiting for 15 minutes before the next update..."
   sleep 900 # Sleep for 15 minutes
 done
---
+++ Scripts/update_search_queries.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Function to cleanup before exiting
+cleanup() {
+  echo "Cleanup complete. Exiting."
+  exit 0
+}
+
+# Trap SIGINT to call the cleanup function on script termination
+trap cleanup SIGINT
+
+# Pull the latest changes from the repository
+git pull --rebase origin main
+
+# Navigate to the data directory
+cd data || { echo "Data directory not found. Exiting."; exit 1; }
+
+# Loop through each subfolder in the data directory
+for dir in */ ; do
+  # Check if search_name.txt exists in the subfolder
+  if [[ -f "$dir/search_name.txt" ]]; then
+    # Read the first line of search_name.txt to use as the commit message
+    commit_message=$(head -n 1 "$dir/search_name.txt")
+    # Add only the modified files in the subfolder to the staging area
+    git add "$dir"*
+    # Commit the changes with the message from search_name.txt
+    git commit -m "Updated query for $commit_message"
+  else
+    # If search_name.txt is not found, use a generic commit message
+    git add "$dir"*
+    git commit -m "Updated query in data/$dir"
+  fi
+done
+
+# Push the changes to the remote repository
+git push origin main
+
+
